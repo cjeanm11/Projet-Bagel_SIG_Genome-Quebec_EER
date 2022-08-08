df7b3663952f161d6c5b47e738961a66fc12884d
MehdiAq <44654549+MehdiAq@users.noreply.github.com>
2022-08-07 23:41:32 -0400

    Refactored & completed CRUD operations for markers

--- api/src/controllers/map.controller.js
+++ api/src/controllers/map.controller.js
@@ -3,16 +3,19 @@ const MapService = require('../services/map.service');
 const mapService = new MapService()
 
 module.exports = {
-    getMap: function(req, res) {
-        res.json(mapService.getMap())
-    },
-    getMarkers: function(req, res) {
-        res.json(mapService.getMarkers())
-    },
-    postMarkers: function(req, res) {
-        res.json(mapService.postMarker(req))
-    },
-    putMarkers: function(req, res) {
-        res.json(mapService.putMarkers(req))
-    }
+  // getMap: function (req, res) {
+  //   res.json(mapService.getMap())
+  // },
+  getMarkers: function (req, res) {
+    res.json(mapService.getMarkers(req, res))
+  },
+  addMarker: function (req, res) {
+    res.json(mapService.addMarker(req, res))
+  },
+  updateMarker: function (req, res) {
+    res.json(mapService.updateMarker(req, res))
+  },
+  deleteMarker: function (req, res) {
+    res.json(mapService.deleteMarker(req, res))
+  }
 }
--- api/src/services/map.service.js
+++ api/src/services/map.service.js
@@ -1,21 +1,52 @@
-class MapService{
-    constructor() {}
+class MapService {
+  constructor() { }
 
-    getMap = () => {
-        return {}
+  // getMap = (req, res) => {
+  //   return {}
+  // }
+
+  getMarkers = (req, res) => {
+    try {
+      const markers = await Marker.find();
+      res.status(200).send({ success: true, data: markers });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send({ success: false, msg: error.message });
     }
-    
-    getMarkers = () => {
-        return [ [51.505, -0.09], [51.530, -0.09] ]
+  }
+
+  addMarker = (req, res) => {
+    try {
+      const markerToSave = req.body.marker;
+      const savedMarker = await Marker.create(markerToSave);
+      res.status(200).send(savedMarker);
     }
-    
-    postMarker = () =>  {
-        return {}
+    catch (error) {
+      console.log(error.message);
+      res.status(500).send({ success: false, msg: error.message });
     }
-    
-    putMarkers = () => {
-        return {}
+  }
+
+  updateMarker = (req, res) => {
+    try {
+      const savedMarker = await Marker.findOneAndReplace({ _id: req.params.id }, req.body.marker)
+      res.status(200).send(savedMarker);
     }
+    catch (error) {
+      console.log(error.message);
+      res.status(500).send({ success: false, msg: error.message });
+    }
+  }
+
+  deleteMarker = (req, res) => {
+    try {
+      await User.findByIdAndRemove(req.params.id);
+      res.status(200).send({ success: true });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send({ success: false, msg: error.message });
+    }
+  }
 }
 
 module.exports = MapService
