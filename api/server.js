3a690c79ed6e5526f0468d08f4970cf7e19ce8bc
MehdiAq <44654549+MehdiAq@users.noreply.github.com>
2022-08-08 23:39:12 -0400

    Updated server.js

--- api/server.js
+++ api/server.js
@@ -9,7 +9,8 @@ const saltRounds = 10;
 const { v4: uuid } = require("uuid");
 require("dotenv").config({ path: __dirname + "/config/.env" });
 const User = require("./src/models/User");
-const Observation = require("./src/models/Observation");
+const Marker = require("./src/models/Marker");
+// const Result = require("./src/models/Result");
 
 const app = express();
 const port = 5000;
@@ -36,149 +37,226 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// User registration
-app.post("/users/signup", async (request, response) => {
+// User sign up
+app.post("/users/signup", async (req, res) => {
+  // const userToSave = req.body.user;
   const userToSave = {
-    id: uuid(),
-    prenom: request.body.prenom,
-    nom: request.body.prenom,
-    email: request.body.email,
-    password: request.body.password,
-    role: request.body.role ? request.body.role : "Élève"
-    // ecole: request.body.school,
+    // id: uuid(),
+    prenom: req.body.prenom,
+    nom: req.body.prenom,
+    identifiant: req.body.identifiant,
+    motDePasse: req.body.motDePasse,
+    role: req.body.role ? req.body.role : "Élève"
+    // ecole: req.body.school,
   };
   try {
-    if (
-      validator.isEmail(userToSave.email)
-      //  && validator.isStrongPassword(userToSave.password)
+    if (true
+      //  validator.isStrongPassword(userToSave.motDePasse)
     ) {
-      // Check to see if the user already exists. If not, then create it.
-      const user = await User.findOne({ email: userToSave.email });
+      // Vérifier que l'identifiant n'est pas déjà utilisé
+      const user = await User.findOne({ identifiant: userToSave.identifiant });
       if (user) {
         const msg =
-          "Un compte est déjà inscrit avec cet email: " + userToSave.email;
+          "Un compte est déjà inscrit avec cet identifiant: " + userToSave.identifiant;
         console.log(msg);
-        response.send({ success: false, msg: msg });
+        res.status(400).send({ success: false, msg: msg });
         return;
       } else {
         const hashedPassword = await bcrypt.hash(
-          userToSave.password,
-          saltRounds
+          userToSave.motDePasse,
+          10
         );
         userToSave.password = hashedPassword;
         const savedUser = await User.create(userToSave);
-        const msg = "Compte créé avec succès";
-        response.send({
+        res.status(200).send({
           success: true,
-          msg: msg,
+          msg: "Compte créé avec succès",
           data: savedUser,
         });
-        return;
       }
     }
+    else {
+      // identifiant ou mot de passe invalide
+      res.status(400).send({ success: false, msg: "Identifiant ou mot de passe invalide" });
+    }
   } catch (error) {
-    console.log("Database error: " + error.message);
-    response.send({ success: false, msg: error.message });
-
+    console.log(error.message);
+    res.status(500).send({ success: false, msg: error.message });
   }
 });
 
-// User login
-app.post("/users/signin", async (request, response) => {
-  const email = request.body.email;
-  const password = request.body.password;
+// User sign in
+app.post("/users/signin", async (req, res) => {
+  const identifiant = req.body.identifiant;
+  const motDePasse = req.body.motDePasse;
   try {
-    if (email && password) {
+    if (identifiant && motDePasse) {
       // Check to see if the user already exists. If not, then create it.
-      const user = await User.findOne({ email: email });
+      const user = await User.findOne({ identifiant: identifiant });
       if (!user) {
-        const msg = "Email invalide: " + email;
+        const msg = "Identifiant invalide: " + identifiant;
         console.log(msg);
-        response.send({ success: false, msg: msg });
+        res.status(400).send({ success: false, msg: msg });
         return;
       } else {
-        const isSame = await bcrypt.compare(password, user.password);
+        const isSame = await bcrypt.compare(motDePasse, user.motDePasse);
         if (isSame) {
           const msg = "Authentification réussi";
           console.log(msg);
-          response.send({ success: true, msg: msg, data: user });
+          res.status(200).send({ success: true, msg: msg, data: user });
           return;
         } else {
           const msg = "Mot de passe erroné";
           console.log(msg);
-          response.send({ success: false, msg: msg });
+          res.status(400).send({ success: false, msg: msg });
           return;
         }
       }
     }
   } catch (error) {
-    console.log("Database error: " + error.message);
-    response.send({ success: false, msg: error.message });
+    console.log(error.message);
+    res.status(500).send({ success: false, msg: error.message });
   }
 });
 
 // Get all users
-app.get("/users", async (request, response) => {
+app.get("/users", async (req, res) => {
   try {
     const users = await User.find();
-    response.send(users);
+    res.status(200).send({ success: true, data: users });
   } catch (error) {
     console.log(error.message);
+    res.status(500).send({ success: false, msg: error.message });
+  }
+});
+
+// Get all students
+app.get("/users/students", async (req, res) => {
+  try {
+    const students = await User.find({ role: "Élève" });
+    res.status(200).send({ success: true, data: students });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ success: false, msg: error.message });
   }
 });
 
 // Get all teachers
-app.get("/users/students", async (request, response) => {
+app.get("/users/teachers", async (req, res) => {
   try {
-    const users = await User.find({ role: "Enseignant" });
-    response.send(users);
+    const teachers = await User.find({ role: "Enseignant" });
+    res.status(200).send({ success: true, data: teachers });
   } catch (error) {
     console.log(error.message);
+    res.status(500).send({ success: false, msg: error.message });
   }
 });
 
-// Get all students
-app.get("/users/teachers", async (request, response) => {
+// Get all admins
+app.get("/users/admins", async (req, res) => {
   try {
-    const users = await User.find({ role: "Élève" });
-    response.send(users);
+    const admins = await User.find({ role: "admin" });
+    res.status(200).send({ success: true, data: admins });
   } catch (error) {
     console.log(error.message);
+    res.status(500).send({ success: false, msg: error.message });
   }
 });
 
-// Get all students
-app.get("/users/admin", async (request, response) => {
+// Get a User by id
+app.get("/users/:id", async (req, res) => {
   try {
-    const users = await User.find({ role: "Élève" });
-    response.send(users);
+    const user = await User.findById(req.params.id);
+    res.status(200).send({ success: true, data: user });
   } catch (error) {
     console.log(error.message);
+    res.status(500).send({ success: false, msg: error.message });
   }
 });
 
-// Get a User by email
-app.post("/users", async (request, response) => {
-  const email = request.body.email;
+// Update a User by id
+app.put("/users/:id", async (req, res) => {
   try {
-    const user = await User.findOne({ email: email });
-    response.send(user);
+    const hashedPassword = await bcrypt.hash(
+      req.body.password,
+      10
+    );
+    // const user = await User.findOneAndReplace({ _id: req.params.id }, req.body.user)
+    const user = await User.findById(req.params.id);
+    user.prenom = req.body.prenom;
+    user.nom = req.body.prenom;
+    user.identifiant = req.body.identifiant;
+    user.password = hashedPassword;
+    user.role = req.body.role ? req.body.role : "Élève";
+    const savedUser = await user.save();
+    res.status(200).send({
+      success: true,
+      msg: "Compte mis à jour avec succès",
+      data: savedUser,
+    });
   } catch (error) {
     console.log(error.message);
+    res.status(500).send({ success: false, msg: error.message });
   }
 });
 
-
-app.post("/observations", async (request, response) => {
-
+// Delete a User by id
+app.delete("/users/:id", async (req, res) => {
   try {
-    const user = await User.findOne({ email: email });
-    response.send(user);
+    await User.findByIdAndRemove(req.params.id);
+    res.status(200).send({ success: true });
   } catch (error) {
     console.log(error.message);
+    res.status(500).send({ success: false, msg: error.message });
   }
 });
 
+// Get all markers
+app.get("/map/markers", async (req, res) => {
+  try {
+    const markers = await Marker.find();
+    res.status(200).send({ success: true, data: markers });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ success: false, msg: error.message });
+  }
+});
+
+// Add a marker
+app.post("/map/markers", (req, res) => {
+  try {
+    const markerToSave = req.body.marker;
+    const savedMarker = await Marker.create(markerToSave);
+    res.status(200).send(savedMarker);
+  }
+  catch (error) {
+    console.log(error.message);
+    res.status(500).send({ success: false, msg: error.message });
+  }
+});
+
+// Update a marker by id
+app.put("/map/markers/:id", (req, res) => {
+  try {
+    const savedMarker = await Marker.findOneAndReplace({ _id: req.params.id }, req.body.marker)
+    res.status(200).send(savedMarker);
+  }
+  catch (error) {
+    console.log(error.message);
+    res.status(500).send({ success: false, msg: error.message });
+  }
+});
+
+// Delete a marker by id
+app.delete("/map/markers/:id", async (req, res) => {
+  try {
+    await User.findByIdAndRemove(req.params.id);
+    res.status(200).send({ success: true });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ success: false, msg: error.message });
+  }
+});
 
 app.listen(port, () =>
   console.log(`GIS app listening on port ${port}!`)
