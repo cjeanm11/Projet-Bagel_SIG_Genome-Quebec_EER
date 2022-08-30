7d8baf0e1f4cfe06b9e451e77654587445fb7534
MehdiAq <44654549+MehdiAq@users.noreply.github.com>
2022-08-29 21:29:26 -0400

    Updated logos and icons

--- client/src/components/map/utils/constants.js
+++ client/src/components/map/utils/constants.js
@@ -4,9 +4,30 @@ import icon from 'leaflet/dist/images/marker-icon.png';
 
 import iconShadow from 'leaflet/dist/images/marker-icon.png';
 
-export const DefaultIconMarker = L.icon({
-    iconUrl: "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|28bc7d&chf=a,s,ee00FFFF",
+export const withResultsIconMarker = L.icon({
+  iconUrl: "https://chart.apis.google.com/chart?chst=d_map_pin_icon&chld=info|69fe93",
 });
-export const altIconMarker = L.icon({
-    iconUrl: "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|34ebc3&chf=a,s,ee00FFFF",
+export const defaultIconMarker = L.icon({
+  iconUrl: "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|69fe93&chf=a,s,ee00FFFF",
+});
+export const editableIconMarker = L.icon({
+  iconUrl: "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|ec1c24&chf=a,s,ee00FFFF",
+});
+export const editableWithResultsIconMarker = L.icon({
+  iconUrl: "https://chart.apis.google.com/chart?chst=d_map_pin_icon&chld=info|ec1c24",
+});
+export const newIconMarker = L.icon({
+  iconUrl: "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|eb797d&chf=a,s,ee00FFFF",
+});
+export const maeGreenIcon = L.icon({
+  iconUrl: "./maeGreenIcon.png",
+  iconSize: [38, 95], // size of the icon
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+export const maeRedIcon = L.icon({
+  iconUrl: "./maeRedIcon.png",
+});
+export const maeLightRedIcon = L.icon({
+  iconUrl: "./maeLightRedIcon.png",
 });
