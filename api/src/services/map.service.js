f4263a294cd2858f3467e37d8b2ed28d46803181
MehdiAq <44654549+MehdiAq@users.noreply.github.com>
2022-08-29 21:27:38 -0400

    Updated services

--- api/src/services/map.service.js
+++ api/src/services/map.service.js
@@ -1,24 +1,28 @@
 const Marker = require("../models/Marker");
+const UsersService = require('../services/users.service');
 
 class MapService {
   constructor() { }
-
-  // getMap() {
-  //   return {}
-  // }
-
   async getMarkers() {
-    return await Marker.find();
+    const markers = await Marker.find().lean();
+    const usersService = new UsersService();
+    for (const marker of markers) {
+      marker.user = await usersService.getUser(marker.userId);
+    }
+    return markers;
   }
-
+  async getMarker(id) {
+    return await Marker.findOne(id);
+  }
   async addMarker(markerToSave) {
     return await Marker.create(markerToSave);
   }
-
-  async updateMarker(markerToUpdate) {
-    return await Marker.findOneAndReplace({ _id: markerToUpdate._id }, markerToUpdate)
+  async updateMarker(updatedMarker) {
+    await Marker.findOneAndUpdate({ _id: updatedMarker._id }, updatedMarker);
   }
-
+  async addResults(markerId, results) {
+    await Marker.findOneAndUpdate({ _id: markerId }, { resultats: results });
+  }
   async deleteMarker(id) {
     await Marker.findByIdAndRemove(id);
   }
--- api/src/services/users.service.js
+++ api/src/services/users.service.js
@@ -1,6 +1,6 @@
-const validator = require("validator");
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const AccessCodesService = require("../services/accessCodes.service");
 
 class UsersService {
   constructor() { }
@@ -10,7 +10,7 @@ class UsersService {
   }
 
   async getUser(id) {
-    return await User.findById(req.params.id);
+    return await User.findById(id);
   }
 
   async getStudents() {
@@ -25,20 +25,43 @@ class UsersService {
     return await User.find({ role: "admin" });
   }
 
-  async signUpUser(userToSignUp) {
-    // Vérifier que le mot de passe est assez fort et que l'identifiant est assez long
-    // if (userToSignUp.identifiant.length < 6 || !validator.isStrongPassword(userToSignUp.motDePasse))
-    //   throw Error("Identifiant ou mot de passe invalide");
+  async signUpUser(userToSignUp, accessCode) {
 
     // Vérifier que l'identifiant n'est pas déjà utilisé
     const user = await User.findOne({ identifiant: userToSignUp.identifiant });
     if (user) throw Error("Un compte est déjà inscrit avec cet identifiant: " + userToSignUp.identifiant);
+
+    // Définir le rôle et la classe de l'usager
+    const accessCodesService = new AccessCodesService()
+    const accessCodeInfo = await accessCodesService.getAccessCodeInfo(accessCode);
+    if (!accessCodeInfo) throw new Error("Le code d'accès est invalide");
+    userToSignUp.role = accessCodeInfo.role;
+    userToSignUp.ecole = accessCodeInfo.ecole;
+    userToSignUp.centreDeServiceScolaire = accessCodeInfo.centreDeServiceScolaire;
+    userToSignUp.niveauScolaire = accessCodeInfo.niveauScolaire;
+
+    // Vérifier que le mot de passe et l'identifiant sont assez longs et ne contiennent pas d'espace
+    if (userToSignUp.identifiant.length < 6)
+      throw Error("L'identifiant doit avoir au minimum 6 caractères");
+    if (userToSignUp.motDePasse.length < 8)
+      throw Error("Le mot de passe doit avoir au minimum 8 caractères");
+    if (userToSignUp.identifiant.includes(' ') || userToSignUp.motDePasse.includes(' '))
+      throw Error("L'identifiant et le mot de passe ne peuvent pas contenir d'espace");
+    // Vérifier que le mot de passe est sécuritaire pour les admins et les enseignants
+    // if (["Admin", "Enseignant"].includes(userToSignUp.role) &&
+    //   !validator.isStrongPassword(userToSignUp.motDePasse))
+    //   throw Error("Le mot de passe doit contenir:\n" +
+    //     "- 1 lettre minuscule\n" +
+    //     "- 1 lettre majuscule\n" +
+    //     "- 1 chiffre\n" +
+    //     "- 1 symbole");
 
     // Encrypter le mot de passe
     userToSignUp.motDePasse = await bcrypt.hash(
       userToSignUp.motDePasse,
       10
     );
+
     return await User.create(userToSignUp);
   }
 
@@ -46,7 +69,7 @@ class UsersService {
     // Vérifier que l'utilisateur existe
     const user = await User.findOne({ identifiant: identifiant });
     if (!user) throw Error("Identifiant invalide: " + identifiant);
-    
+
     // Vérifier que le mode de passe est correct
     const isSame = await bcrypt.compare(motDePasse, user.motDePasse);
     if (!isSame) throw Error("Mot de passe erroné");
